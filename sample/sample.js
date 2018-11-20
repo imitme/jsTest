a58fe91d78d8c17bb5e98fd18942a9fa5325e857
kyeongmin <7kyeongmin@gmail.com>
2018-11-20 12:16:48 +0900

    testCode

---
+++ sample/sample.js
@@ -0,0 +1,29 @@
+// var i = 3;
+// let name = "홍길동"
+// let age = 30;
+// console.log(age);
+
+// let name = "rudals";
+// let len = name.length;
+// console.log(len);
+// name.replace ('rud', '경');
+// console.log(name);
+// let newName = name.replace ('rud', '경');
+// console.log(newName);
+
+// let char = newName.charAt(3);
+// console.log(char);
+
+// console.log(newName.toUpperCase());
+// console.log(newName.toLowerCase());
+
+//----------------------------------------------
+// var x = 0;
+// x += 5;
+// console.log(x);
+// console.log('hello ' + 'world');
+// console.log('1' + 2 + 3 + 4);
+
+
+console.log(123 == '123');
+console.log(123 === '123');
