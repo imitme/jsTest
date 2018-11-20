681e8dd3bf2de634fbaaf76f439450a3ae9f89ac
kyeongmin <7kyeongmin@gmail.com>
2018-11-20 14:57:24 +0900

    object 실습

---
+++ sample/object.js
@@ -0,0 +1,15 @@
+//var obj = new Object();
+
+var obj = {};
+obj.name = "minn";
+console.log(obj.name);
+
+var obj2 = new Object();
+obj2["name"] = "홍길동";
+console.log(obj2.name);
+
+var person = {
+    name : "홍길동",
+    age : 33,
+    address : {zipcode: 14500, city: "서울"}
+}
