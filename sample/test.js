f22fd9b107916de7667b4a3a889e34c6dbcb5b21
kyeongmin <7kyeongmin@gmail.com>
2018-11-20 14:10:26 +0900

    test js 추가

---
+++ sample/test.js
@@ -0,0 +1,45 @@
+// var name = 'Min';
+// if(name == 'min'){
+//     console.log("My name is min.");
+// } else if (name == 'Min'){
+//     console.log("My name is Min.");
+// }
+
+// for( var i = 0; i < 5 ; i++){
+//     console.log("i = %d", i);
+// }
+
+// let time1 = 100 ; 
+// while ( time1 > 100){
+//     console.log("Result: &d", time1);
+//     time1--;
+// }
+
+// let time2 = 100;
+// do{
+//     console.log("Result : %d", time2);
+//     time2--;
+//     console.log("Result : %d", time2);
+// }while (time2 > 100)
+
+// var name = 'min';
+// if(name == 'mmin'){
+//     console.log("My name is mmin.");
+// } else if (name == 'min'){
+//     console.log("My name is min.");
+// } else if (name == 'Min'){
+//     console.log("My name is Min.");
+// }
+
+let action = "eat" ;
+switch(action){
+    case 'draw':
+        console.log('draw');
+        break;
+    case 'eat' :
+        console.log('eat');
+        break;
+    default:
+        console.log('nothing');
+}
+
